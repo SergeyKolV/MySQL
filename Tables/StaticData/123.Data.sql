59e31dc541e555285012d4c079111f8ea4f26095
Sergey Kolesnik <SergeyKol@datasoft.local>
2024-04-16 17:18:39 +0300

    4

---
+++ Tables/StaticData/123.Data.sql
@@ -0,0 +1,6 @@
+﻿INSERT INTO `123`(id, name, f_name) VALUES
+(1, 'qq', 'qqq');
+INSERT INTO `123`(id, name, f_name) VALUES
+(2, 'ww', 'www');
+INSERT INTO `123`(id, name, f_name) VALUES
+(3, 'ee', 'eee');
