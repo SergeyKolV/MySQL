e3602f160cb84783ff5a003f469ad2e50fe88b78
Sergey Kolesnik <SergeyKol@datasoft.local>
2024-04-18 13:04:02 +0300

    5

---
+++ Procedures/test.sql
@@ -0,0 +1,23 @@
+﻿DELIMITER $$
+
+CREATE PROCEDURE `test`(IN test int)
+BEGIN
+  SELECT
+    test;
+  SELECT
+    test;
+  CREATE TEMPORARY TABLE temp_test (
+    test_column int
+  );
+  INSERT INTO temp_test (test_column)
+    VALUE (test);
+  SELECT
+    test;
+  SELECT
+    test;
+  SELECT
+    test;
+END
+$$
+
+DELIMITER ;
