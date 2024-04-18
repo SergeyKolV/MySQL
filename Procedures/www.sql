e038501872726b1ea867dd7a7ba8d960bbef2744
Sergey Kolesnik <SergeyKol@datasoft.local>
2024-04-18 14:47:15 +0300

    8

---
+++ Procedures/www.sql
@@ -0,0 +1,20 @@
+﻿DELIMITER $$
+
+CREATE PROCEDURE `www`(IN test INT)
+BEGIN
+  SELECT
+    test;
+  SELECT
+    test;
+  CREATE TEMPORARY TABLE temp_test (
+    test_column int
+  );
+  INSERT INTO temp_test (test_column)
+    VALUE (test);
+  SELECT
+    test;
+
+END
+$$
+
+DELIMITER ;
