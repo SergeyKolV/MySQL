c6a8f54e875662a84e3ec736e8fc41cf1365bec1
SergeyKolV <126674726+SergeyKolV@users.noreply.github.com>
2024-08-16 12:35:05 +0300

    Update данные.sql

--- Tables/данные.sql
+++ Tables/данные.sql
@@ -1,8 +1,9 @@
 ﻿CREATE TABLE `данные` (
   `id` INT DEFAULT NULL,
-  `name` VARCHAR(50) DEFAULT NULL
+  `name` VARCHAR(50) DEFAULT NULL,
+   `id` INT
 )
 ENGINE = INNODB,
 CHARACTER SET utf8mb4,
 COLLATE utf8mb4_0900_ai_ci;
-========
+
