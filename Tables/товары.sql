453ff248a0df452573536526dab1ec06fb8d2e52
SergeyKolV <126674726+SergeyKolV@users.noreply.github.com>
2024-08-16 12:43:40 +0300

    Update товары.sql

--- Tables/товары.sql
+++ Tables/товары.sql
@@ -1,8 +1,1 @@
-﻿CREATE TABLE `товары` (
-  `код` INT NOT NULL,
-  `название` VARCHAR(255) DEFAULT NULL,
-  PRIMARY KEY (код)
-)
-ENGINE = INNODB,
-CHARACTER SET utf8mb4,
-COLLATE utf8mb4_0900_ai_ci;
+
